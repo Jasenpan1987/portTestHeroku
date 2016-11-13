243d9426e1e5c924875857b1e9018c98d1d5151c
Jasen Pan <jasenpan@gmail.com>
2016-11-14 02:12:45 +1100

    2nd commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express()
 var PORT = process.env.PORT||3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World! from '+PORT)
 })
 
 app.listen(PORT, function () {
